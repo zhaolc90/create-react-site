2084d112132d3cb6d4a8f0f95b5e5ed5d4765754
c <ytmssr@outlook.com>
2018-10-10 08:11:26 +0800

    init

---
+++ src/main/webapp/precache-manifest.2127b67d4baf25eb973b453f9f270dbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/playbook/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f6d07c51e4bd3728fbcd",
+    "url": "/playbook/static/js/runtime~main.f6d07c51.js"
+  },
+  {
+    "revision": "23f250113d66719525cb",
+    "url": "/playbook/static/js/main.23f25011.chunk.js"
+  },
+  {
+    "revision": "e960ffa73a05df59f914",
+    "url": "/playbook/static/js/1.e960ffa7.chunk.js"
+  },
+  {
+    "revision": "23f250113d66719525cb",
+    "url": "/playbook/static/css/main.f6e41468.chunk.css"
+  },
+  {
+    "revision": "84447450a4abe2eebf33efbd641e3f5e",
+    "url": "/playbook/index.html"
+  }
+];
