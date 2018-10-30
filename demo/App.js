8d5ac02074b8594f6a08385503b4ac309c642bae
c <ytmssr@outlook.com>
2018-10-30 21:59:51 +0800

    init

---
+++ demo/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+const defaultProps = {};
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                1
+            </React.Fragment>
+        );
+    }
+}
+
+ App.propTypes = propTypes;
+ App.defaultProps = defaultProps;
